ba07bb37c294b616f580cd6e396a484b45db5313
lesurfeur <lesurfeur72@gmail.com>
2013-12-16 18:13:28 +0100

    Create admin_image_group.php

---
+++ Mod/Image-Group/files/lang/French/admin_image_group.php
@@ -0,0 +1,52 @@
+<?php
+
+// Language definitions used in Image Award Plugin
+$lang_admin_image_group = array(
+
+'plugin name'               =>  'Image Groupe',
+'plugin desc'               =>  'Ce plugin permet de configurer les options d\'affichage des images de groupe.',
+'options'                   =>  'Options',
+'settings'			        =>	'Réglages',
+'display'			        =>	'Affichage',
+'display up'			    =>	'Au-dessus de l\'avatar',
+'display down'			    =>	'Sous l\'avatar',
+'display choice'			=>	'Choisissez où placer les images de groupe.',
+'directory'                 =>	'Répertoire image de groupe',
+'directory folder'          =>	'Chemin du dossier où les images de groupe seront envoyés (chemin relatif au dossier racine de FluxBB). PHP doit avoir les droits en écriture dans ce répertoire.',
+'width'                     =>	'Largeur maximum',
+'height'                    =>	'Hauteur maximum',
+'width comment'             =>	'La largeur maximum admise (exprimée en pixel) pour les images de groupe (60 est recommandé).',
+'height comment'            =>	'La hauteur maximum admise (exprimée en pixel) pour les images de groupe (30 est recommandé).',
+'size'                      =>	'Taille maximum',
+'size comment'              =>	'La taille maximum admise (exprimée en octets) pour les images de groupe (10240 est recommandé).',
+'save'			         	=>	'Enregistrer',
+'success option'            =>  'Options Image de Groupe mises à jour. Redirection ...',
+
+
+// Language definitions used in admin_groups.php
+
+'Image legend'				=>	'Réglages des images de groupe',
+'No image'					=>	'Aucune image définie',
+'Image upload redirect'		=>	'Image téléchargée. Redirection ...',
+'Image deleted redirect'	=>	'Image effacée. Redirection ...',
+'Image desc'				=>	'Une image de groupe remplacera l\'affichage textuel du nom du groupe dans les sujets. Il ne doit pas faire plus de',
+'Upload image'				=>	'Télécharger une image',
+'Upload image legend'		=>	'Sélectionnez un fichier image à télécharger',
+'Delete image'				=>	'Effacer l\'image',	// only for admins
+'Image info'				=>	'Une image de groupe remplacera l\'affichage textuel du nom du groupe dans les sujets. Vous pouvez télécharger une image en cliquant sur le lien ci-dessous. La case \'Utiliser l\'image\' doit être cochée pour que l\'image soit visible dans les sujets.',
+'Change image'				=>	'Modifier image',
+'Use image'					=>	'Utiliser l\'image.',
+'Too large ini'				=>	'Fichier trop volumineux. Le serveur refuse son envoi.',
+'Partial upload'			=>	'Fichier partiellement téléchargé. Merci d\'essayer à nouveau.',
+'No tmp directory'			=>	'Le serveur n\'a pas pu enregistrer le fichier envoyé dans un répertoire temporaire.',
+'No file'					=>	'Vous n\'avez pas sélectionné de fichier à envoyer.',
+'Bad type'					=>	'Le type du fichier n\'est pas autorisé. Les types autorisés sont le GIF, le JPG et le PNG.',
+'Too wide or high'			=>	'L\'image est plus large et/ou plus haute que le maximum autorisé',
+'Too large'					=>	'La taille du fichier dépasse le maximum autorisé',
+'pixels'					=>	'pixels',
+'bytes'						=>	'octets',
+'Move failed'				=>	'Le serveur n\'a pas pu enregistrer le fichier envoyé. Contactez l\'administrateur',
+'Unknown failure'			=>	'Erreur inconnue. Merci d\'essayer à nouveau.',
+'File'						=>	'Fichier',
+'Upload'					=>	'Télécharger'	// submit button
+);
